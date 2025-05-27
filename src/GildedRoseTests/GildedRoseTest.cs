b50db70624c0f04fb0b0be2717123114cd916294
Ben White <bewhite87@gmail.com>
2025-05-27 20:15:38 +0100

    Refactor GildedRoseTest to improve item quality test
    Removed the Item_ShouldHaveSellInProperty method and added
    UpdateQuality_DecreasesSellInAndQualityByOne_ForNormalItem
    to test the UpdateQuality function for normal items. The new
    test verifies that both SellIn and Quality values decrease
    correctly, enhancing clarity and purpose.

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -1,24 +1,29 @@
 ﻿using GildedRoseKata;
-using System.Reflection;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
     /// <summary>
-    /// All items have a SellIn value which denotes the number of days we have to sell the items
-    /// All items have a Quality value which denotes how valuable the item is
+    /// At the end of each day our system lowers both values for every item
     /// </summary>
-    [Theory]
-    [InlineData("SellIn")]
-    [InlineData("Quality")]
-    public void Item_ShouldHaveSellInProperty(string propName)
+    [Fact]
+    public void UpdateQuality_DecreasesSellInAndQualityByOne_ForNormalItem()
     {
-        // Act
-        PropertyInfo propInfo = typeof(Item)
-            .GetProperty(propName);
+        // Given the item has a sell in value of 10 and a quality value of 20
+        Item item = new()
+        {
+            Name = "Test Item",
+            SellIn = 10,
+            Quality = 20
+        };
+        GildedRose sut = new([item]);
 
-        // Assert
-        Assert.NotNull(propInfo);
+        // When UpdateQuality is invoked
+        sut.UpdateQuality();
+
+        // Then the sell in value should be 9 and the quality value should be 19
+        Assert.Equal(9, item.SellIn);
+        Assert.Equal(19, item.Quality);
     }
 }
