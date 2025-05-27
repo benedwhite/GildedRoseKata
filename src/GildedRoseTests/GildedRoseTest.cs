b32b725f22f9b1f07a3c2abf3c67e4aa3d5d7255
Ben White <bewhite87@gmail.com>
2025-05-27 20:40:18 +0100

    Refactor GildedRose tests for clarity and maintainability
    Updated test methods to use expected value variables instead of hardcoded assertions. This change improves readability and enhances the clarity of the tests' intent.

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -16,13 +16,15 @@ public class GildedRoseTest
         // Given the item has a sell in value of 10 and a quality value of 20
         Item item = CreateItem("Test Item", 10, quality: 20);
         GildedRose sut = CreateGuildedRose(item);
+        int expectedSellIn = 9;
+        int expectedQuality = 19;
 
         // When UpdateQuality is invoked
         sut.UpdateQuality();
 
         // Then the sell in value should be 9 and the quality value should be 19
-        Assert.Equal(9, item.SellIn);
-        Assert.Equal(19, item.Quality);
+        Assert.Equal(expectedSellIn, item.SellIn);
+        Assert.Equal(expectedQuality, item.Quality);
     }
 
     /// <summary>
@@ -36,12 +38,13 @@ public class GildedRoseTest
         // Given the item sell by date has passed (0 or negative)
         Item item = CreateItem("Test Item", sellIn, quality: 20);
         GildedRose sut = CreateGuildedRose(item);
+        int expectedQuality = 18;
 
         // When UpdateQuality is invoked
         sut.UpdateQuality();
 
         // Then the quality degrades twice as fast
-        Assert.Equal(18, item.Quality);
+        Assert.Equal(expectedQuality, item.Quality);
     }
 
     private static Item CreateItem(string name, int sellIn, int quality)
