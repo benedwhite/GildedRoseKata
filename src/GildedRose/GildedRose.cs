ccc1541ea0f45a91dd37404b70b610092c01d8f6
Ben White <bewhite87@gmail.com>
2025-05-28 07:05:24 +0100

    Simplify quality increase conditions for Backstage passes
    Refactor the logic in the GildedRose class to streamline the conditions for increasing the quality of "Backstage passes to a TAFKAL80ETC concert". Removed redundant checks for "Aged Brie" and clarified the conditions based on `SellIn` values (less than 11 and 6) while ensuring `Quality` remains below 50. This improves code readability and maintainability.

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -23,18 +23,14 @@ public class GildedRose(IList<Item> items)
                 item.Quality = item.Quality + 1;
             }
 
-            if ((item.Name == "Aged Brie"
-                || item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                && item.Name == "Backstage passes to a TAFKAL80ETC concert"
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert"
                 && item.SellIn < 11
                 && item.Quality < 50)
             {
                 item.Quality = item.Quality + 1;
             }
 
-            if ((item.Name == "Aged Brie"
-                || item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                && item.Name == "Backstage passes to a TAFKAL80ETC concert"
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert"
                 && item.SellIn < 6
                 && item.Quality < 50)
             {
