3c35abe2970a7b304951985b6da860e1beb800e9
Ben White <bewhite87@gmail.com>
2025-05-31 13:05:56 +0100

    Add unit tests for AgedBrieUpdater functionality
    Introduce `AgedBrieUpdaterTests` class with unit tests to verify the behavior of the `AgedBrieUpdater`. Tests include checks for null items, sell-in value decrement, and quality increase under various conditions. A helper method `CreateAgedBrieUpdater` is added for easier instance creation in tests.

---
+++ src/GildedRoseTests/Updaters/AgedBrieUpdaterTests.cs
@@ -0,0 +1,67 @@
+﻿using GildedRoseKata;
+using GildedRoseKata.Updaters;
+
+namespace GildedRoseTests.Updaters;
+public class AgedBrieUpdaterTests : GildedRoseTestBase
+{
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_WhenItemIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AgedBrieUpdater(null));
+    }
+
+    [Fact]
+    public void Update_ShouldDecreaseSellIn_WhenCalled()
+    {
+        // Arrange
+        Item item = CreateItem("Aged Brie", sellIn: 20, quality: 20);
+        AgedBrieUpdater updater = CreateAgedBrieUpdater(item);
+
+        // Act
+        updater.Update();
+
+        // Assert
+        Assert.Equal(19, item.SellIn);
+    }
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(49, 50)]
+    [InlineData(50, 50)]
+    public void Update_ShouldIncreaseQualityByOne_GivenAnItemWithQualityLessThan50AndSellInGreaterThanZero(
+        int quality,
+        int expectedQuality)
+    {
+        // Arrange
+        Item item = CreateItem("Aged Brie", sellIn: 1, quality);
+        AgedBrieUpdater updater = CreateAgedBrieUpdater(item);
+
+        // Act
+        updater.Update();
+
+        // Assert
+        Assert.Equal(expectedQuality, item.Quality);
+    }
+
+    [Theory]
+    [InlineData(1, 3)]
+    [InlineData(48, 50)]
+    [InlineData(49, 50)]
+    [InlineData(50, 50)]
+    public void Update_ShouldIncreaseQualityByTwoToNoMoreThan50_GivenAnItemWithQualityLessThan50AndSellInLessThanOrEqualToZero(
+        int quality,
+        int expectedQuality)
+    {
+        // Arrange
+        Item item = CreateItem("Aged Brie", sellIn: 0, quality);
+        AgedBrieUpdater updater = CreateAgedBrieUpdater(item);
+
+        // Act
+        updater.Update();
+
+        // Assert
+        Assert.Equal(expectedQuality, item.Quality);
+    }
+
+    private static AgedBrieUpdater CreateAgedBrieUpdater(Item item) => new(item);
+}
