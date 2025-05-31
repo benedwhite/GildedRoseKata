7978128b21a2ad26ae99657979c210e1da879fdd
Ben White <bewhite87@gmail.com>
2025-05-31 13:34:29 +0100

    Add Items class to Constants for item name constants
    Introduced a new nested static class `Items` within the `Constants` class. This class contains four constant string fields: `AgedBrie`, `BackstagePass`, `Sulfuras`, and `Conjured`, representing various item names used in the Gilded Rose Kata.

--- src/GildedRose/Constants.cs
+++ src/GildedRose/Constants.cs
@@ -2,4 +2,12 @@
 public static class Constants
 {
     public static readonly int MaxQuality = 50;
+    
+    public static class Items
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string Conjured = "Conjured Mana Cake";
+    }
 }
